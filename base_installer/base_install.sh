1d9c3f9b879fe4e473a0b497c825c3527edae0a7
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2025-12-07 14:22:36 +0100

    complete rewrite of old base-install.sh, now modularized

---
+++ base_installer/base_install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash -e
+
+# do not continue unless run as root
+if [ $UID -ne 0 ]; then
+	echo "Please run this program as root or using sudo."
+	exit 1
+fi
+
+# check if our config file exists
+if ! test -s ./base_install.conf ; then
+	echo "Config file ./base_install.conf not found!"
+	exit 1
+fi
+
+# check for media presence
+while ! (test -L /dev/disk/by-label/bootfs && \
+	 test -L /dev/disk/by-label/rootfs); do
+	echo "No partitions labelled bootfs and rootfs found."
+	echo "Try removing and reinserting the media."
+	echo "Sleeping 30 seconds and trying again ..."
+	sleep 30
+done
+
+# source our config variables
+# TODO check that all required variables are set
+. ./base_install.conf
+
+#set dynamic variables
+export MOUNTPOINT=$(mktemp -d)
+export TEMPIMAGEDIR=$(mktemp -d -p $TEMPIMAGEDIRROOT)
+
+# set BASEDEV and PARTITIONxyz variables depending on removable storage type
+export BASEDEV=$(realpath /dev/disk/by-label/bootfs | sed -e 's/[0-9]$//')
+if echo -n "$BASEDEV" | grep -q "mmc" ; then
+	export BASEDEV=$(echo -n "$BASEDEV" | sed -e 's/p$//')
+	export PARTONE="${BASEDEV}p1"
+	export PARTTWO="${BASEDEV}p2"
+	export PARTTHREE="${BASEDEV}p3"
+	export PARTFIVE="${BASEDEV}p5"
+	export PARTSIX="${BASEDEV}p6"
+	export PARTSEVEN="${BASEDEV}p7"
+	export PARTEIGHT="${BASEDEV}p8"
+else
+	export PARTONE="${BASEDEV}1"
+	export PARTTWO="${BASEDEV}2"
+	export PARTTHREE="${BASEDEV}3"
+	export PARTFIVE="${BASEDEV}5"
+	export PARTSIX="${BASEDEV}6"
+	export PARTSEVEN="${BASEDEV}7"
+	export PARTEIGHT="${BASEDEV}8"
+fi
+
+# now run our configuration steps
+if test -d ./base_install_scripts.d ; then
+        run-parts --exit-on-error ./base_install_scripts.d
+else
+	echo "Directory ./base_install_scripts.d not found!"
+	exit 1
+fi
+
+####################
+#
+
+# TODO
+# - try mkfs and rsync -avPAHXx --numeric-ids origin destination/ instead of dd - is it faster?
+# - investigate why dnsmasq/ip config of ENV1 wouldn't start up at first boot - might be a flaky cable
+# - Make ENV1,2,3 do a "reboot dance" straight after base install, ending up in ENV2 - work in progress
+# - maybe use partx instead of partprobe? or no partprobe at all ...
+# - raspi-config nonint enable_overlayfs
+# - update-rc.d alsa-utils disable
+# - update-rc.d dnsmasq disable
+# was sagt wdctl?
+# was passiert bei einem minimalsten Linux (direkt vom imager), nur mit watchdog?
+# mount /dev/sda7 /mnt/
+# mount /dev/sda3 /mnt/boot/firmware/
+# mount --bind /dev /mnt/dev
+# mount -t devpts none /mnt/dev/pts
+# mount -t proc none /mnt/proc
+# mount --bind /sys /mnt/sys
+# chroot /mnt/
+# apt install --allow-remove-essential sysvinit-core initscripts orphan-sysvinit-scripts rsyslog watchdog ntpsec-ntpdate uuid netcat-openbsd fake-hwclock systemd-sysv- systemd- apparmor- bluez-
+# apt purge systemd-timesyncd systemd-zram-generator network-manager cloud-init
+# apt autopurge -y
+# 
