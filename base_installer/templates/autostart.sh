d484adaf980bb2d522d67dc12c4a0eb40f41b858
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2025-12-08 17:36:39 +0100

    big cleanup, also now finishes what the ENV-scripts start

--- base_installer/templates/autostart.sh
+++ base_installer/templates/autostart.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-# test if cloud-init is still running (generic homedir not renamed/moved yet)
-if ! [ -d /home/$(grep -A1 "^users:" /boot/firmware/user-data | awk '$2=="name:" { print $3 }') ]; then
-	echo "cloud-init incomplete $(date)" >>/data/reboot.log
-	exit 0
-fi
-chvt 8
+# test if cloud-init is still running (generic homedir not renamed/moved yet) #runonce
+if ! [ -d /home/$(grep -A1 "^users:" /boot/firmware/user-data | awk '$2=="name:" { print $3 }') ]; then #runonce
+	echo "cloud-init incomplete $(date)" >>/data/reboot.log #runonce
+	exit 0 #runonce
+fi #runonce
 
-while ! test -s /etc/ssh/banner; do clear >/dev/tty8 ; echo "banner not set yet $(date)" >/dev/tty8 ; sleep 10; done
+# switch to console #8 and show that we're not yet ready if the banner is not set yet
+while ! test -s /etc/ssh/banner; do chvt 8; clear >/dev/tty8 ; echo "banner not set yet $(date)" >/dev/tty8 ; sleep 10; done
+chvt 1
 
-echo "$(cat /etc/ssh/banner) - $(date)">>/data/reboot.log
+# log our ENV and date
+echo "$(cat /etc/ssh/banner) - bootup complete - $(date)">>/data/reboot.log
 
 # is this the second reboot (first one does not log our ENV)? then remove cloud-init and set reboot cycle to ENV2 #runonce
 if [ $(grep $(cat /etc/ssh/banner) /data/reboot.log | wc -l) -eq 1 ] ; then #runonce
-	## removal currently disabled #runonce
-	## remove cloud-init #runonce
-	#apt purge cloud-init -y && apt autopurge -y #runonce
+	# remove cloud-init #runonce
+	apt purge cloud-init -y #runonce
+	# do not use apt autopurge -y or apt clean here, or you might wipe the overlayfs packages we already downloaded during the chroot phase #runonce
 	# enable overlay file system #runonce
-	# sed -e "s#overlayroot=tmpfs #overlayroot=tmpfs:recurse=0 #" -i /boot/firmware/cmdline.txt #runonce
-	# set reboot cycle to next ENV #runonce
-	# echo "Date is now: \$(date)" | tee -a /data/reboot.log >/dev/tty8 #runonce
-	# /sbin/shutdown -r 1 1 2>&1 | tee -a /data/reboot.log >/dev/tty8 #runonce
+	raspi-config nonint enable_overlayfs #runonce
+	# make sure /data is not affected by overlayfs #runonce
+	sed -e "s#overlayroot=tmpfs #overlayroot=tmpfs:recurse=0 #" -i /boot/firmware/cmdline.txt #runonce
 	if grep -q "^ENV1" /etc/ssh/banner; then #runonce
+		apt clean && apt autopurge -y #runonce
 		sed -e "s#^boot_partition=1#boot_partition=2#" -i /boot/firmware/autoboot.txt #runonce
 		echo "Date is now: $(date)" | tee -a /data/reboot.log >/dev/tty8 #runonce
 		if /sbin/shutdown -r 1 1 2>&1 | tee -a /data/reboot.log >/dev/tty8; then #runonce
@@ -30,22 +32,29 @@ if [ $(grep $(cat /etc/ssh/banner) /data/reboot.log | wc -l) -eq 1 ] ; then #run
 			touch /data/ENV1-could-not-reboot #runonce
 		fi #runonce
 	elif grep -q "^ENV2" /etc/ssh/banner; then #runonce
+		# as we already downloaded the required packages during the chroot phase, we can install sl without needing internet access #runonce
+		apt install -y sl #runonce
+		apt clean && apt autopurge -y #runonce
 		mount /dev/disk/by-label/bootfs /mnt #runonce
 		sed -e "s#^boot_partition=2#boot_partition=3#" -i /mnt/autoboot.txt #runonce
 		umount /dev/disk/by-label/bootfs #runonce
 		echo "Date is now: $(date)" | tee -a /data/reboot.log >/dev/tty8 #runonce
-		if /sbin/reboot ; then #runonce
+		if /sbin/shutdown -r 1 ; then #runonce
 			touch /data/ENV2-stage-complete #runonce
 		else #runonce
 			touch /data/ENV2-could-not-reboot #runonce
 		fi #runonce
 	elif grep -q "^ENV3" /etc/ssh/banner; then #runonce
+		# as we already downloaded the required packages during the chroot phase, we can install sl without needing internet access #runonce
+		apt install -y sl #runonce
+		apt clean && apt autopurge -y #runonce
 		mount /dev/disk/by-label/bootfs /mnt #runonce
 		sed -e "s#^boot_partition=3#boot_partition=2#" -i /mnt/autoboot.txt #runonce
 		touch /mnt/config_complete.txt #runonce
 		umount /dev/disk/by-label/bootfs #runonce
 		touch /data/ENV3-stage-complete #runonce
 		echo "Date is now: $(date)" | tee -a /data/reboot.log >/dev/tty8 #runonce
+		# this line removes all lines ending with #runonce from this autostart.sh file
 		if sed -e "/#runonce$/d" -i /data/autostart.sh && /sbin/shutdown -r 1 1 2>&1 | tee -a /data/reboot.log >/dev/tty8; then #runonce
 			touch /data/ENV3-runonce-removed #runonce
 		else #runonce
