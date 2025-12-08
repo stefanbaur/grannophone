c43b1de83835002fe61d944fddec04528b931258
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2025-12-08 17:17:20 +0100

    syntax fix

--- base_installer/templates/ENV3-custom-install.sh
+++ base_installer/templates/ENV3-custom-install.sh
@@ -2,4 +2,4 @@
 
 # THIS SCRIPT WILL BE EXECUTED INSIDE THE CHANGEROOT, NO NEED TO CALL chroot HERE
 echo "Downloading the 'sl' package into ENV3, so it can be installed later - even without internet access."
-$MOUNTPOINT apt-get install -d -y sl
+apt-get install -d -y sl
