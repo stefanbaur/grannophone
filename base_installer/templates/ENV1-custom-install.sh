57904fa39943fafce467e16dc4592d294766e76c
Stefan Baur <X2Go-ML-1@baur-itcs.de>
2025-12-08 12:37:43 +0100

    removed unneccessary chroot call

--- base_installer/templates/ENV1-custom-install.sh
+++ base_installer/templates/ENV1-custom-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
+# THIS SCRIPT WILL BE EXECUTED INSIDE THE CHANGEROOT, NO NEED TO CALL chroot HERE
 # run this right before umounting, so there's no apt clean call destroying our work
 # download the packages required for overlayroot, so you don't need internet access to activate it
 echo "Downloading required packages for overlayroot, so it can be installed later - even without internet access."
-chroot $MOUNTPOINT apt-get install -d -y cryptsetup cryptsetup-bin overlayroot
+apt-get install -d -y cryptsetup cryptsetup-bin overlayroot
--- base_installer/templates/ENV3-custom-install.sh
+++ base_installer/templates/ENV3-custom-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+# THIS SCRIPT WILL BE EXECUTED INSIDE THE CHANGEROOT, NO NEED TO CALL chroot HERE
 echo "Downloading the 'sl' package into ENV3, so it can be installed later - even without internet access."
-chroot $MOUNTPOINT apt-get install -d -y sl
+$MOUNTPOINT apt-get install -d -y sl
